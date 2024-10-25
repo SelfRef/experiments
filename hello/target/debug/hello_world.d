bab6326dd0d0da8b59294b469b6567d3f91a3787
SelfRef <git@selfref.dev>
2024-10-25 20:23:55 +0200

    Clean up rust dir

--- hello/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/tm/Documents/Rust/target/debug/hello_world: /home/tm/Documents/Rust/src/main.rs
--- hello/target/rls/debug/deps/hello_world-593fbb042618fcac.d
+++
@@ -1,5 +0,0 @@
-/home/tm/Documents/Rust/hello/target/rls/debug/deps/hello_world-593fbb042618fcac.rmeta: src/main.rs
-
-/home/tm/Documents/Rust/hello/target/rls/debug/deps/hello_world-593fbb042618fcac.d: src/main.rs
-
-src/main.rs:
